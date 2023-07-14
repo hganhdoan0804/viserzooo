bb3497302b1dce9e54dda67205fad5fa6072e8d7
Doan Hoang Anh <98464268+anhakadoan84@users.noreply.github.com>
2023-07-14 17:42:53 +0700

    complete project

---
+++ assets/js/viserZoo.js
@@ -0,0 +1,159 @@
+//Mobile Bar
+var navBarMobile = document.querySelector(".navbar-mobile_btn");
+var navBarMobileList = document.querySelector(".nav-mobile__list");
+
+navBarMobile.onclick = function () {
+    navBarMobileList.classList.toggle("nav-mobile-open");
+}
+
+//Animal section
+const animalCards = document.querySelectorAll(".animal-card-js");
+const animalCardsLength = animalCards.length;
+const animalButtons = document.querySelectorAll(".animals-btn");
+const animalButtonsLength = animalButtons.length;
+// Add active when click button 
+Array.from(animalButtons).forEach(function (element) {
+    element.addEventListener("click", function () {
+        for (var i = 0; i < animalButtonsLength; i++) {
+            animalButtons[i].classList.remove("active");
+        }
+        this.classList.add("active");
+
+        // Get data filter
+        let animalFilter = element.dataset.filter;
+        Array.from(animalCards).forEach(function (item) {
+            let animalItem = item.dataset.item;
+
+            if (animalItem === animalFilter || animalFilter === "all") {
+                item.style.display = "block";
+                item.style.animation = "mobileBarGrowth 0.5s linear"
+            }
+            else {
+                item.style.display = "none";
+            }
+        })
+    })
+})
+// Animal section overlay
+const arrayImage = ["animal-card_img-1", "animal-card_img-2",
+    "animal-card_img-3", "animal-card_img-4",
+    "animal-card_img-5", "animal-card_img-6",
+    "animal-card_img-8", "animal-card_img-9",
+    "animal-card_img-10", "animal-card_img-11",
+    "animal-card_img-12", "animal-card_img-13",
+    "animal-card_img-14", "animal-card_img-15",
+    "animal-card_img-16"];
+const arrayImageLength = arrayImage.length;
+const animalModal = document.querySelector(".animal-modal");
+const animalModalImage = document.querySelector(".animal-modal__img");
+const animalCard = document.querySelectorAll(".animal-card");
+let arrayImageIndex = 0;
+
+// Get animal image name
+Array.from(animalCard).forEach(function (elementCards) {
+    elementCards.addEventListener("click", function () {
+        let firstChildImage = elementCards.firstElementChild.src;
+        let imgIndexNumber = firstChildImage.indexOf("img");
+        let pngIndexNumber = firstChildImage.indexOf(".png");
+        let imageName = firstChildImage.slice(imgIndexNumber + 4, pngIndexNumber);
+        // Show animal image
+        animalModal.style.display = "flex";
+        animalModalImage.style.backgroundImage = `url(/viserzoo/assets/img/${imageName}.png)`;
+        for (let i = 0; i < arrayImageLength; i++) {
+            if (imageName === arrayImage[i]) {
+                arrayImageIndex = i;
+                console.log(arrayImageIndex);
+            }
+        }
+    })
+})
+//Move animal image
+const rightButton = document.querySelector(".arrow-right");
+const leftButton = document.querySelector(".arrow-left");
+rightButton.addEventListener("click", function () {
+    arrayImageIndex++;
+    console.log(arrayImageIndex);
+    if (arrayImageIndex > arrayImageLength - 1) {
+        arrayImageIndex = 0;
+    }
+    animalModalImage.style.backgroundImage = `url(/viserzoo/assets/img/${arrayImage[arrayImageIndex]}.png)`;
+})
+
+leftButton.addEventListener("click", function () {
+    arrayImageIndex--;
+    console.log(arrayImageIndex);
+    if (arrayImageIndex < 0) {
+        arrayImageIndex = arrayImageLength - 1;
+    }
+    animalModalImage.style.backgroundImage = `url(/viserzoo/assets/img/${arrayImage[arrayImageIndex]}.png)`;
+})
+
+//Click to close animal modal
+const animalCloseButton = document.querySelector(".animal-modal__close");
+animalCloseButton.addEventListener("click", function () {
+    animalModal.style.display = "none";
+})
+
+//Interactions
+setTimeout(function () {
+    odometer1.innerHTML = 156;
+}, 100);
+
+setTimeout(function () {
+    odometer2.innerHTML = 2;
+}, 100);
+
+setTimeout(function () {
+    odometer3.innerHTML = 1.5;
+}, 100);
+
+setTimeout(function () {
+    odometer4.innerHTML = 35;
+}, 100);
+
+//Feedback
+$(document).ready(function () {
+    $(".feedback__slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: "<button type='button' class='slick-prev pull-left text-white'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right text-white'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        cssEase: 'ease-in-out',
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: false,
+                    speed: 300,
+                    infinite: false,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: false,
+                    speed: 300,
+                    infinite: false,
+                }
+            },
+        ]
+    });
+});

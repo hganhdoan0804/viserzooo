84f4ed26e6d67e7024e92ac1ef236acf5e912241
Doan Hoang Anh <98464268+anhakadoan84@users.noreply.github.com>
2023-07-14 18:07:17 +0700

    update search, back-to-top and preloader

--- assets/js/viserZoo.js
+++ assets/js/viserZoo.js
@@ -157,3 +157,52 @@ $(document).ready(function () {
         ]
     });
 });
+
+//Back to top button
+var backToTopBtn = document.querySelector(".back-to-top");
+window.onscroll = function () {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        backToTopBtn.style.display = "block";
+    }
+    else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+backToTopBtn.onclick = function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Preloading page
+var loader = document.querySelector(".preloader");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
+
+//Search-btn
+var searchBtns = document.querySelectorAll(".search");
+var modal = document.querySelector(".search-modal");
+var modalForm = document.querySelector(".search-modal__form");
+
+function showSearch() {
+    modal.classList.add("active");
+}
+
+function closeSearch() {
+    modal.classList.remove("active");
+}
+
+//Click search button to show search 
+for (var searchBtn of searchBtns) {
+    searchBtn.addEventListener("click", showSearch);
+}
+
+//Click to hide search
+modal.addEventListener("click", closeSearch);
+
+//Stop Propagation
+modalForm.addEventListener("click", function (event) {
+    event.stopPropagation();
+})
+
